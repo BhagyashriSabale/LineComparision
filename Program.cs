734bbd05a527b2e989c86b7f229d1b3e34b90756
BhagyashriSabale <balisabale16@gmail.com>
2023-06-28 14:42:59 +0530

    [Bhagyashri] Calculate length line of two point

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem");
+            double x1, y1, x2, y2;
+            Console.WriteLine("Please enter value x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter value y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter value x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter value y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of line:" + length);
         }
     }
 }
